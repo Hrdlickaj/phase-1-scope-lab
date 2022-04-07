7be42611b48777acc454a4f8b8c73d757a460a88
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-04-07 10:07:56 -0600

    commit tree 670246683d0b8dff5484f928cea6cc9704df399d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(customerName) {
+    console.log(customerName.toUpperCase());
+    return customerName;
+}
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = 'kelly';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "jean";
+
+function changeLeastFavoriteCustomer(name) {
+    const leastFavoriteCustomer = "sally"
+    return leastFavoriteCustomer
+}
+
+
